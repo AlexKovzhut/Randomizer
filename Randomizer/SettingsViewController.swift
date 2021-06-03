2882bdcfa711951b4b61c1dd40eb087f3b65e096
Alexander Kovzhut <akovzhut@icloud.com>
2021-06-03 23:48:49 +0300

    initial commit

---
+++ Randomizer/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Alexander Kovzhut on 03.06.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
